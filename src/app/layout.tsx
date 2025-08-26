146ba06669393b1ebbbc06a8af371c237d0f9712
Júlio Cesar Gomes <julio.gomes92@gmail.com>
2025-08-26 13:27:28 +0000

    Coloque um fundo no favicon, para que seja fácil visualizar em qualquer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   },
   description: 'Crie e gerencie Cloud Pages para Salesforce Marketing Cloud com uma interface visual intuitiva, componentes reutilizáveis e publicação instantânea.',
   icons: {
-    icon: `data:image/svg+xml;base64,${btoa('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" /><path d="m14 12-4 4 4 4" /><path d="m10 12 4 4" /></svg>')}`,
+    icon: `data:image/svg+xml;base64,${btoa('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="24" height="24" x="0" y="0" fill="#fff" rx="12"></rect><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" /><path d="m14 12-4 4 4 4" /><path d="m10 12 4 4" /></svg>')}`,
   },
 };
 

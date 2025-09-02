ecb4490ec5776451bc05e446014a09b711383521
Júlio Cesar Gomes <julio.gomes92@gmail.com>
2025-09-02 13:42:46 +0000

    Tudo na busca global continua não sendo clicável. Resolva isso

--- src/components/app/command-palette.tsx
+++ src/components/app/command-palette.tsx
@@ -39,9 +39,9 @@ export function CommandPalette() {
     return () => document.removeEventListener("keydown", down);
   }, []);
   
-  const runCommand = (command: () => void) => {
+  const runAndClose = (callback: () => void) => {
     setOpen(false);
-    command();
+    callback();
   };
 
   return (
@@ -52,7 +52,7 @@ export function CommandPalette() {
         <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
         
         <CommandGroup heading="Ações Rápidas">
-          <CommandItem onSelect={() => runCommand(() => router.push('/'))}>
+          <CommandItem onSelect={() => runAndClose(() => router.push('/'))}>
             <Plus className="mr-2 h-4 w-4" />
             <span>Criar Novo Projeto</span>
           </CommandItem>
@@ -61,23 +61,23 @@ export function CommandPalette() {
         <CommandSeparator />
 
         <CommandGroup heading="Navegação">
-          <CommandItem onSelect={() => runCommand(() => router.push('/'))}>
+          <CommandItem onSelect={() => runAndClose(() => router.push('/'))}>
             <Folder className="mr-2 h-4 w-4" />
             <span>Ver Projetos</span>
           </CommandItem>
-          <CommandItem onSelect={() => runCommand(() => router.push('/templates'))}>
+          <CommandItem onSelect={() => runAndClose(() => router.push('/templates'))}>
             <Library className="mr-2 h-4 w-4" />
             <span>Ver Templates</span>
           </CommandItem>
-           <CommandItem onSelect={() => runCommand(() => router.push('/brands'))}>
+           <CommandItem onSelect={() => runAndClose(() => router.push('/brands'))}>
             <Palette className="mr-2 h-4 w-4" />
             <span>Ver Kits de Marca</span>
           </CommandItem>
-          <CommandItem onSelect={() => runCommand(() => router.push('/media'))}>
+          <CommandItem onSelect={() => runAndClose(() => router.push('/media'))}>
             <ImageIcon className="mr-2 h-4 w-4" />
             <span>Ver Biblioteca de Mídia</span>
           </CommandItem>
-          <CommandItem onSelect={() => runCommand(() => router.push('/account'))}>
+          <CommandItem onSelect={() => runAndClose(() => router.push('/account'))}>
             <User className="mr-2 h-4 w-4" />
             <span>Minha Conta</span>
           </CommandItem>
@@ -88,7 +88,7 @@ export function CommandPalette() {
         {projects.length > 0 && (
             <CommandGroup heading="Projetos">
             {projects.map((project) => (
-                <CommandItem key={`project-${project.id}`} onSelect={() => runCommand(() => router.push(`/project/${project.id}`))}>
+                <CommandItem key={`project-${project.id}`} onSelect={() => runAndClose(() => router.push(`/project/${project.id}`))}>
                 <Folder className="mr-2 h-4 w-4" />
                 <span>{project.name}</span>
                 </CommandItem>
@@ -99,7 +99,7 @@ export function CommandPalette() {
         {pages.length > 0 && (
           <CommandGroup heading="Páginas">
             {pages.map((page) => (
-              <CommandItem key={`page-${page.id}`} onSelect={() => runCommand(() => router.push(`/editor/${page.id}`))}>
+              <CommandItem key={`page-${page.id}`} onSelect={() => runAndClose(() => router.push(`/editor/${page.id}`))}>
                 <FileText className="mr-2 h-4 w-4" />
                 <span>{page.name}</span>
               </CommandItem>
@@ -112,7 +112,7 @@ export function CommandPalette() {
                 {allTemplates.map((template) => (
                 <CommandItem key={`template-${template.id}`} onSelect={() => {
                     if (!template.isDefault) {
-                        runCommand(() => router.push(`/editor/${template.id}?isTemplate=true`));
+                        runAndClose(() => router.push(`/editor/${template.id}?isTemplate=true`));
                     }
                 }} disabled={template.isDefault}>
                     <Library className="mr-2 h-4 w-4" />

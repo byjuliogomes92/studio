5bd096595b329d39a35ea0161d29903abe6147fa
Júlio Cesar Gomes <julio.gomes92@gmail.com>
2025-09-04 18:35:42 +0000

    Adição de log para debug de componente DIV

--- src/components/app/settings/component-settings.tsx
+++ src/components/app/settings/component-settings.tsx
@@ -55,6 +55,7 @@ const renderComponentSettings = (
   projectPages: CloudPage[]
 ) => {
     const props = { component, onPropChange, onSubPropChange, projectPages };
+    console.log("TIPO DO COMPONENTE SELECIONADO:", component.type);
     switch (component.type) {
       case "Div": return <DivSettings {...props} />;
       case "Header": return <HeaderSettings {...props} />;
@@ -91,6 +92,7 @@ const renderComponentSettings = (
 }
 
 export function ComponentSettings({ component, onComponentChange, projectPages }: ComponentSettingsProps) {
+  console.log('--- O COMPONENTE "ComponentSettings" RENDERIZOU COM ESTE COMPONENTE:', component);
   const abTestEnabled = component.abTestEnabled || false;
   const variantProps = (component.abTestVariants && component.abTestVariants[0]) || {};
 

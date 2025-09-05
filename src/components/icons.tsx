074f3c819a3d3e686e7634fbbac650ca061a8b4f
Júlio Cesar Gomes <julio.gomes92@gmail.com>
2025-09-05 20:17:32 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,7 +2,7 @@
 import type { ImgHTMLAttributes } from "react";
 import Image from "next/image";
 
-export const Logo = (props: ImgHTMLAttributes<HTMLImageElement>) => (
+export const Logo = (props: Partial<React.ComponentProps<typeof Image>>) => (
   <Image
     src="https://firebasestorage.googleapis.com/v0/b/quizkong-mvp.firebasestorage.app/o/morfeu_logo_icon.svg?alt=media&token=d3b3612f-dc20-4900-aa30-b4376f496bb8"
     alt="Morfeu Logo"

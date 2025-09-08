ae147b198ea44de15f81d06715aa30cff2fb15e1
Júlio Cesar Gomes <julio.gomes92@gmail.com>
2025-09-08 12:01:42 +0000

    Error: Invalid src prop (https://upload.wikimedia.org/wikipedia/commons/

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'mkt.twblocks.com.br',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
